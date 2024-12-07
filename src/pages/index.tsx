ce8834fc2dc35e596776834da810422637ad3c54
Raphael-Ramalho <rsramalhovf@gmail.com>
2024-12-07 15:30:59 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="w-full bg-red-600 h-8 items-center flex pl-2">
-      <span>Header</span>
+    <div className="group">
+      <div className="w-full bg-red-600 h-8 items-center flex pl-2">
+        <span>Header</span>
+      </div>
+      <div className="group/test">
+        <div className="m-2 w-24 h-20 bg-green-500 group-hover/test:bg-fuchsia-600">Container</div>
+        <div className="m-2 w-24 h-20 bg-green-500">Container 2</div>
+      </div>
     </div>
   );
 }
